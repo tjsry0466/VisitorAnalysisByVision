567e13cb252b7235d6c4559beba80ee9fca41b1f
jeong seonkyo <tjs0466@naver.com>
2021-05-16 18:51:37 +0900

    refactoring 3 - separate prediction file

---
+++ prediction.py
@@ -0,0 +1,51 @@
+import cv2
+import numpy as np
+from yolov5.utils.general import non_max_suppression
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
+from tensorflow.keras.preprocessing.image import img_to_array
+
+class Predictions():
+    def __init__(self, yl_model):
+        self.yl_model = yl_model 
+
+    def predict_object(self, img):
+        pred = self.yl_model(img, augment=True)[0]
+        pred = non_max_suppression(pred, 0.4, 0.5, classes=[0], agnostic=True)[0]
+        return pred
+
+    def detect_and_predict_mask(self, frame, faceNet, maskNet):
+        ref_confidence = 0.5
+
+        (h, w) = frame.shape[:2]
+        blob = cv2.dnn.blobFromImage(frame, 1.0, (300, 300),
+            (104.0, 177.0, 123.0))
+
+        faceNet.setInput(blob)
+        detections = faceNet.forward()
+
+        faces = []
+        locs = []
+        preds = []
+        for i in range(0, detections.shape[2]):
+            confidence = detections[0, 0, i, 2]
+            if confidence > ref_confidence:
+                box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+                (startX, startY, endX, endY) = box.astype("int")
+
+                (startX, startY) = (max(0, startX), max(0, startY))
+                (endX, endY) = (min(w - 1, endX), min(h - 1, endY))
+
+                face = frame[startY:endY, startX:endX]
+                face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
+                face = cv2.resize(face, (224, 224))
+                face = img_to_array(face)
+                face = preprocess_input(face)
+
+                faces.append(face)
+                locs.append((startX, startY, endX, endY))
+
+        if len(faces) > 0:
+            faces = np.array(faces, dtype="float32")
+            preds = maskNet.predict(faces, batch_size=32)
+
+        return (locs, preds)   
--- track.py
+++ track.py
@@ -6,26 +6,31 @@ import cv2
 import torch
 
 from yolov5.utils.datasets import LoadStreams
-from yolov5.utils.general import non_max_suppression
 
 from config import Models
+from prediction import Predictions
 from draws import draw_boxes, draw_face_and_mask_area
-from detactions import detect_and_predict_mask
 from dpsort import deepsort_input
 from processing import classify_face, s3_face_upload
 from etc import convert_tensor_xywh        
 
 def detect():
     fdb = {}
+
     models = Models()
     deepsort = models.get_deepsort_model()
-    model, device = models.get_yolov5_model()
+    yl_model, device = models.get_yolov5_model()
     faceNet = models.get_face_model()
     maskNet = models.get_mask_model()
 
-    dataset = LoadStreams('0', img_size=640)
+    prediction = Predictions(yl_model)
+
+    # https://github.com/ultralytics/yolov5/blob/master/utils/datasets.py
+    dataset = LoadStreams('0', img_size=640) # self.sources, img, img0, None
     
     for frame_idx, (path, img, im0s, vid_cap) in enumerate(dataset):
+        im0 = im0s[0].copy()
+        im0c = im0.copy()
         img = torch.from_numpy(img).to(device)
         img = img.half() 
         img /= 255.0  # 0 - 255 to 0.0 - 1.0
@@ -33,19 +38,14 @@ def detect():
             img = img.unsqueeze(0)
 
         # object detaction
-        pred = model(img, augment=True)[0]
-        pred = non_max_suppression(pred, 0.4, 0.5, classes=[0], agnostic=True)
+        pred_obj = prediction.predict_object(img)
 
         # face and mask detaction
-        im0 = im0s[0].copy()
-        im0c = im0.copy()
-        (locs, preds) = detect_and_predict_mask(im0, faceNet, maskNet)
+        locs, preds = prediction.detect_and_predict_mask(im0, faceNet, maskNet)
 
         # iterate object detection result
-        obj_ = pred[0]
-        
-        if len(obj_):
-            bbox_xywh, confs = convert_tensor_xywh(img, im0,  obj_)
+        if len(pred_obj):
+            bbox_xywh, confs = convert_tensor_xywh(img, im0,  pred_obj)
             outputs = deepsort_input(im0, bbox_xywh, confs, deepsort)
             if len(outputs) > 0:
                 draw_boxes(im0, outputs)
